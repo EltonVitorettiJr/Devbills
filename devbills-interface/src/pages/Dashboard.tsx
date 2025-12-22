bf29d4daa7098b2c8118f52f787f5ea3cf9eb5ca
EltonVitorettiJr <eltonvitorettijr@gmail.com>
2025-12-22 13:35:48 -0300

    Criado a lógica de filtros na tela de dashboard (Mês e ano)

--- devbills-interface/src/pages/Dashboard.tsx
+++ devbills-interface/src/pages/Dashboard.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import MonthYearSelect from "../components/MonthYearSelect";
+import { api } from "../services/api";
+
 const Dashboard = () => {
+  const currentDate = new Date();
+  // por padrão, jan é o mês 0
+  const [month, setMonth] = useState<number>(currentDate.getMonth() + 1);
+  const [year, setYear] = useState<number>(currentDate.getFullYear());
+
+  useEffect(() => {
+    async function getTransactions() {
+      const result = await api.get("/transactions");
+
+      console.log(result);
+    }
+
+    getTransactions();
+  }, []);
+
   return (
-    <div>
-      <h1>Olá, Dashboard!</h1>
+    <div className="container-app py-6">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+        <h1 className="text-2xl font-bold mb-4 md:mb-0">Dashboard</h1>
+        <MonthYearSelect
+          month={month}
+          year={year}
+          onMonthChange={setMonth}
+          onYearChange={setYear}
+        />
+      </div>
     </div>
   );
 };
